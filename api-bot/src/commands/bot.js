ab6a9aac29abc1e878faae0b936ab7adc9ad165f
vitaliyirtlach <w13vitaliy@gmail.com>
2021-07-02 22:28:20 +0300

    feat: user settings profile

--- api-bot/src/commands/bot.js
+++ api-bot/src/commands/bot.js
@@ -11,44 +11,46 @@ module.exports = {
         try {
             const botId = args[0]
             const res = await fetch(`https://bots.topcord.ru/api/${botId}`)
-            if (res.status === 429) return message.channel.send(`Too Many Requests`)
+            if (res.status === 429)
+                return message.channel.send(`Too Many Requests`)
 
             const info = await res.json()
-            if (info.error == 'Unknown bot') return message.channel.send(`Бот \`${bot}\` не найден.`)
-            
+            if (info.error == 'Unknown bot')
+                return message.channel.send(`Бот \`${bot}\` не найден.`)
+
             const bot = await client.users.fetch(botId)
             const avatar = `https://cdn.discordapp.com/avatars/${bot}/${bot.avatar}.png?size=1280`
             console.log(info)
             const embed = new MessageEmbed()
-            .setTitle(info.bot.username)
-            .setDescription(
-                `\`\`\`json\n${info.shortDesc}\`\`\`\nРазработчик: <@${info.owner.id}>\n Апов: **${info.upvotes}**\nПрефикс: **${info.prefix}**\n Бот добавлен на сайт: **${info.date.addedAt}** \n Бот создан: **${info.date.createdAt}**`
-            )
-            embed.setFooter(`Теги: ${info.botTags.join(", ")}.`)
+                .setTitle(info.bot.username)
+                .setDescription(
+                    `\`\`\`json\n${info.shortDesc}\`\`\`\nРазработчик: <@${info.owner.id}>\n Апов: **${info.upvotes}**\nПрефикс: **${info.prefix}**\n Бот добавлен на сайт: **${info.date.addedAt}** \n Бот создан: **${info.date.createdAt}**`
+                )
+            embed.setFooter(`Теги: ${info.botTags.join(', ')}.`)
             embed.setColor('#7289da')
             embed.setThumbnail(avatar)
 
             const button = new MessageButton()
-            .setStyle('url')
-            .setURL(`https://topcord.ru/bots/${botId}`)
-            .setLabel('Бот на TopCord')
-            .setEmoji('858744916720353340')
+                .setStyle('url')
+                .setURL(`https://topcord.ru/bots/${botId}`)
+                .setLabel('Бот на TopCord')
+                .setEmoji('858744916720353340')
 
             const button2 = new MessageButton()
-            .setStyle('url')
-            .setURL(info.customInvite)
-            .setLabel('Пригласить Бота')
-            .setEmoji('858744623055503370')
+                .setStyle('url')
+                .setURL(info.customInvite)
+                .setLabel('Пригласить Бота')
+                .setEmoji('858744623055503370')
 
             const button3 = new MessageButton()
-            .setStyle('url')
-            .setURL(info.botWebsite)
-            .setLabel('Сайт')
-            .setEmoji('858761530770456606')
+                .setStyle('url')
+                .setURL(info.botWebsite)
+                .setLabel('Сайт')
+                .setEmoji('858761530770456606')
 
             const row = new MessageActionRow()
-            .addComponent(button)
-            .addComponent(button2)
+                .addComponent(button)
+                .addComponent(button2)
 
             info.botWebsite && row.addComponent(button3)
 
