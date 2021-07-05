3f7211dff0bdc939602fa5a7516113ad5315cb7f
vitaliyirtlach <w13vitaliy@gmail.com>
2021-07-05 18:48:37 +0300

    delete .env file

--- web/config/index.ts
+++ web/config/index.ts
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
- 
+
 export default {
-    SERVER_URL: isDev ? 'http://localhost:5000' : 'https://api-bots.topcord.ru'
+    SERVER_URL: "http://localhost:5000"
 }
