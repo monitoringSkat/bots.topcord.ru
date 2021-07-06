dac1ca86851d63325fb1fcd1409971326e4cee4f
d1m0s23 <dimashow12388@gmail.com>
2021-07-06 13:25:18 +0300

    404 page

--- web/pages/404.tsx
+++ web/pages/404.tsx
@@ -1,3 +1,13 @@
-const PageNotFound = () => <div>Page not Found</div>
+import styles from '../styles/pages/404.module.scss'
+import Layout from '../layout'
+
+const PageNotFound = () => (
+    <Layout title="404 | Topcord">
+        <div className={styles.box} >
+            <h1 className={styles.text} >404</h1>
+            <h3 className={styles.undertext} >Страница не найдена.</h3>
+        </div>
+    </Layout> 
+     )
 
 export default PageNotFound
