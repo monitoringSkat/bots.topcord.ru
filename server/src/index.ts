a801541fd2828da94f347403b9fb0a7d132e91b4
vitaliyirtlach <w13vitaliy@gmail.com>
2021-08-10 22:17:28 +0300

    fix: errors with build application typeorm entities and createConnection object

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,17 +16,31 @@ import tagsRouter from './routes/tags.route'
 import cookieParser from 'cookie-parser'
 import DiscordJS from 'discord.js'
 import User from './entities/User'
+import BlackList from './entities/BlackList'
+import Bot from './entities/Bot'
+import Comment from './entities/Comment'
+import Tag from './entities/Tag'
 
 dotenv.config()
 const PORT = Number(process.env.PORT || 5000)
-const __prod__ = process.env.NODE_ENV === "development"
+const __prod__ = process.env.NODE_ENV === 'development'
 
 const corsOptions = {
-    origin: __prod__ ? 'http://localhost:3000' : "https://dev.topcord.ru/",
+    origin: __prod__ ? 'http://localhost:3000' : 'https://dev.topcord.ru',
     optionsSuccessStatus: 200
 }
 
-orm.createConnection()
+orm.createConnection({
+    "type": "postgres",
+    "host": "localhost",
+    "port": 5432,
+    "username": "postgres",
+    "password": "13092005",
+    "database": "topcord",
+    "synchronize": true,
+    "logging": false,
+    "entities": [User, BlackList, Bot, Comment, Tag]
+})
     .then(async () => {
         const app = express()
         const upvotes = new DiscordJS.Collection<string, number>()
