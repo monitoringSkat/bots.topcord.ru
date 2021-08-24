846919ea6a76cbfed8ddc5bac182111ba0c975ee
d1m0s23 <dimashow12388@gmail.com>
2021-08-25 01:37:20 +0300

    пробелы

--- web/src/layout/index.tsx
+++ web/src/layout/index.tsx
@@ -55,20 +55,20 @@ const Layout: FC<Props> = ({ children, title, description, image }) => (
             <script
             dangerouslySetInnerHTML={{
               __html: `
-<script async type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                <script async type="text/javascript" >
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(70435240, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/70435240" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-`
+                ym(70435240, "init", {
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true
+                });
+                </script>
+                <noscript><div><img src="https://mc.yandex.ru/watch/70435240" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+                `
             }}
             />
             <link
