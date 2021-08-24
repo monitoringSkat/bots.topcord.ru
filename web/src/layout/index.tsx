33e1cd1c7810e0c4f5c2ad9f3ed984347aa4c929
d1m0s23 <dimashow12388@gmail.com>
2021-08-25 01:33:22 +0300

    Update index.tsx

--- web/src/layout/index.tsx
+++ web/src/layout/index.tsx
@@ -52,6 +52,25 @@ const Layout: FC<Props> = ({ children, title, description, image }) => (
               </script>`
             }}
             />
+            <script
+            dangerouslySetInnerHTML={{
+              __html: `
+<script async type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(70435240, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/70435240" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+`
+            }}
+            />
             <link
                 rel="shortcut icon"
                 href="/assets/favicon.png"
