dc2c27f3e04b90f0da479d7259733c7b3314821d
d1m0s23 <61113540+d1m0s23@users.noreply.github.com>
2021-08-29 00:54:12 +0300

    Update cancel-approve.ts

--- server/src/bot/commands/cancel-approve.ts
+++ server/src/bot/commands/cancel-approve.ts
@@ -26,6 +26,7 @@ const cancelApprove: Command = {
         const embedmember = new MessageEmbed()
             .addField('Причина', reason.join(' ') || 'Причины нету')
             .addField('Бот', bot.name)
+            .setDescription('**Когда вы исправите причину, уведомите об этом модераторов, чтобы они перепроверили вашего бота по новой!!!**')
             .setThumbnail(bot.avatar)
             .setTimestamp()
             .setTitle('Ваш бот был отклонен')
