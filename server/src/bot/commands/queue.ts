f3e2f00d7e381e628b42d9ffd995705d88abb19c
d1m0s23 <61113540+d1m0s23@users.noreply.github.com>
2021-08-29 00:33:04 +0300

    Update queue.ts

--- server/src/bot/commands/queue.ts
+++ server/src/bot/commands/queue.ts
@@ -23,11 +23,8 @@ const approve: Command = {
             .setTitle("Очередь")
             .addFields(
                 { name: 'ID', value: bots.map((bot) => bot.id), inline: true },
-                { name: '\u200B', value: '\u200B' },
                 { name: 'Name', value: bots.map((bot) => bot.name), inline: true },
-                { name: '\u200B', value: '\u200B' },
                 { name: 'Prefix', value: bots.map((bot) => bot.prefix), inline: true },
-                { name: '\u200B', value: '\u200B' },
                 { name: 'Invite', value: bots.map((bot) => bot.inviteURL), inline: true },
             )
             .setColor(" #2F3136")
