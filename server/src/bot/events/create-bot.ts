611077665aaec25feab595fea1e03d86c42e0111
d1m0s23 <61113540+d1m0s23@users.noreply.github.com>
2021-08-29 01:18:07 +0300

    Update create-bot.ts

--- server/src/bot/events/create-bot.ts
+++ server/src/bot/events/create-bot.ts
@@ -18,7 +18,10 @@ const createBot: Event = {
             .setTitle('Добавлен бот')
             .setColor('#008000')
         const channel: any = client.channels.cache.get(parsed.BOT_ADD_CHANNEL)
+        const modchannel: any = client.channels.cache.get(parsed.BOT_ADD_CHANNEL)
+        modchannel.send("<@761596581145083914> Бот " + bot.name + " был добавлен. **t!queue " + bot.id + "**" )
         channel.send(embed)
+        
     }
 }
 
